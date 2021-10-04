703d9af9022e62318388ef02c9a6553d974fb5ed
Progitiel <github@progiciel.be>
2021-10-04 16:10:09 +0200

    footer

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="py-4">
-    <div class="text-center text-secondary dark:text-dark-secondary">
-      2021 ©
-      <a href="https://progiciel.be" class="text-primary">Progiciel</a>
+  <div class="py-4 flex flex-col items-center gap-4">
+    <div class="flex gap-4 text-secondary dark:text-dark-secondary">
+      <nuxt-link :to="localePath('legal')">Legal</nuxt-link>
+      <nuxt-link :to="localePath('privacy')">Privacy</nuxt-link>
     </div>
+    <p class="text-secondary-light dark:text-dark-secondary-light">
+      2021 © Internet Society Chapter Belgium.
+      {{ $t('footer.made_by') }}
+      <a href="https://progiciel.be" class="underline">Progiciel</a>
+    </p>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({})
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
   theme: {
     colors: {
       primary: '#0077FF', // HSB(210, 100, 100)
-      secondary: '#19191A', // HSB(210, 2, 10)
-      'dark-secondary': '#EDF0F2', // HSB(210, 2, 95)
+      secondary: {
+        DEFAULT: '#19191A', // HSB(210, 2, 10)
+        light: '#7D7E80', // HSB(210, 2, 50)
+      },
+      'dark-secondary': {
+        DEFAULT: '#EDF0F2', // HSB(210, 2, 95)
+        light: '#898B8C', // HSB(210, 2, 55)
+      },
       container: '#FAFCFF', // HSB(210, 2, 100)
       'dark-container': '#292E33', // HSB(210, 20, 20)
       'secondary-container': '#E1E3E6', // HSB(210, 2, 90)
