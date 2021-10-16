3f1c1997375cd0314611bf0aa2f81bfcdb5d1ece
Frederic Taes <github@tsf.be>
2021-10-16 12:18:59 +0200

    translation: get add-on NL

--- lang/nl.js
+++ lang/nl.js
@@ -3,7 +3,7 @@ export default {
     new_release: 'Nieuwe versie',
     days_ago: 'vandaag | {days} dag geleden | {days} dagen geleden',
     description: 'De betrouwbaarheid van elke website verifiëren',
-    get_the_addon: 'Koop de add-on',
+    get_the_addon: 'Add-on toevoegen',
     download: {
       title: 'Download',
       description: 'Alle beschikbare platforms',
