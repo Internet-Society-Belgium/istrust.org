705a1f7ac35cd6e66f508642bfd7369ce04a356b
Progitiel <github@progiciel.be>
2021-12-28 08:50:51 +0100

    teaser:  real

---
+++ teaser/src/main/sequences/Real.tsx
@@ -0,0 +1,17 @@
+import {BrowserTitleBar} from '../components/BrowserTitleBar';
+
+export const Real: React.FC = () => {
+	return (
+		<div className="flex flex-col justify-center items-center gap-10">
+			<BrowserTitleBar
+				url={new URL('https://www.istrust.org/')}
+				extension={{}}
+			/>
+			<h3 className="text-2xl text-secondary-light">OR</h3>
+			<BrowserTitleBar
+				url={new URL('https://www.is-trust.org/')}
+				extension={{}}
+			/>
+		</div>
+	);
+};
