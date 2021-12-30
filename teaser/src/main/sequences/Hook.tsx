11be1089b1ea68f285fa42abe454a5aa941d50a1
Progitiel <github@progiciel.be>
2021-12-30 11:58:54 +0100

    teaser: hacker

--- teaser/src/main/sequences/Hook.tsx
+++ teaser/src/main/sequences/Hook.tsx
@@ -1,9 +1,13 @@
+import Lottie from 'lottie-react';
 import {BrowserTitleBar} from '../components/BrowserTitleBar';
+
+import hacker from '../../../public/animations/hacker.json';
 
 export const Hook: React.FC = () => {
 	return (
 		<div className="flex flex-col justify-center items-center gap-10">
 			<BrowserTitleBar typing />
+			<Lottie animationData={hacker} />
 		</div>
 	);
 };
